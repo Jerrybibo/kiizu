5184b3a027e456ab9b3ce67f1d359f58d101e3a4
Jerrybibo <59254806+Jerrybibo@users.noreply.github.com>
2021-07-28 03:14:10 -0400

    Set up word list, key detection, Pygame clock

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import pygame as p
-from constants import *
 from pygame.locals import *
+from random import choice
+
+from constants import *
+from helpers import create_text
 
 window_surface = p.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 p.display.set_caption("Kiizu - for Raymond", APP_ICON)
 
+try:
+    WORD_LIST_FILE = open("resources/word_list.txt")
+    WORD_LIST = [i.rstrip("\n") for i in WORD_LIST_FILE.readlines()]
+    WORD_LIST_FILE.close()
+except OSError as err:
+    print("OSError: {0}".format(err))
+
+pressed_keys = []
+current_word = ""
+
+clock = p.time.Clock()
 
 def menu():
-    menu_background_color = BLACK
+    menu_background_color = MINT
     while 1:
         window_surface.fill(menu_background_color)
-
         for event in p.event.get():
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
@@ -26,10 +39,14 @@ def menu():
 
 
 def main():
+    global pressed_keys, current_word
     menu()
+    word_complete = True
     while 1:
-        window_surface.fill(BLACK)
-
+        window_surface.fill(BLUE)
+        pressed_keys = p.key.get_pressed()
+        if word_complete:
+            current_word = choice(WORD_LIST)
         for event in p.event.get():
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
@@ -40,6 +57,7 @@ def main():
                 exit(0)
 
         p.display.update()
+        clock.tick(60)
 
 
 main()
